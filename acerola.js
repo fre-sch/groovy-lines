d527a3d8cef3d166aac3e09ac5f0d272162771ff
Frederik Schumacher <fre-sch@users.noreply.github.com>
2023-10-04 13:34:53 +0200

    init

---
+++ acerola.js
@@ -0,0 +1,99 @@
+// lifted from https://acerola.gg/colors.html
+// accompanying video: https://youtu.be/fv-wlo8yVhk?si=OXdFctDREaR1m7M9
+
+
+function PaletteSettings (COLOR_COUNT) {
+  return {
+    hueBase: Math.random(),
+    hueContrast: Math.min(1, Math.abs(randomGaussian(1, 4))),
+    saturationBase: Math.min(1, Math.abs(randomGaussian(1, 4))),
+    saturationContrast: Math.random(),
+    luminanceBase: Math.random(),
+    luminanceContrast: Math.random(),
+    fixed: Math.random(),
+    saturationConstant: true,
+    colorCount: COLOR_COUNT,
+  }
+}
+
+function oklch_to_oklab(L, c, h) {
+  return [L, c * Math.cos(h), c * Math.sin(h)]
+}
+
+function oklab_to_linear_srgb(L, a, b) {
+  let l_ = L + 0.3963377774 * a + 0.2158037573 * b
+  let m_ = L - 0.1055613458 * a - 0.0638541728 * b
+  let s_ = L - 0.0894841775 * a - 1.291485548 * b
+
+  let l = l_ * l_ * l_
+  let m = m_ * m_ * m_
+  let s = s_ * s_ * s_
+
+  return [
+    +4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s,
+    -1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s,
+    -0.0041960863 * l - 0.7034186147 * m + 1.707614701 * s,
+  ]
+}
+
+function generateOKLCH(HUE_MODE, settings) {
+  let oklchColors = []
+
+  let hueBase = settings.hueBase * 2 * Math.PI
+  let hueContrast = lerp(0.33, 1.0, settings.hueContrast)
+
+  let chromaBase = lerp(0.01, 0.1, settings.saturationBase)
+  let chromaContrast = lerp(
+    0.075,
+    0.125 - chromaBase,
+    settings.saturationContrast
+  )
+  let chromaFixed = lerp(0.01, 0.125, settings.fixed)
+
+  let lightnessBase = lerp(0.3, 0.6, settings.luminanceBase)
+  let lightnessContrast = lerp(
+    0.3,
+    1.0 - lightnessBase,
+    settings.luminanceContrast
+  )
+  let lightnessFixed = lerp(0.6, 0.9, settings.fixed)
+
+  let chromaConstant = settings.saturationConstant
+  let lightnessConstant = !chromaConstant
+
+  if (HUE_MODE == "monochromatic") {
+    chromaConstant = false
+    lightnessConstant = false
+  }
+
+  for (let i = 0; i < settings.colorCount; ++i) {
+    let linearIterator = i / (settings.colorCount - 1)
+
+    let hueOffset = linearIterator * hueContrast * 2 * Math.PI + Math.PI / 4
+
+    if (HUE_MODE == "monochromatic") hueOffset *= 0.0
+    if (HUE_MODE == "analagous") hueOffset *= 0.25
+    if (HUE_MODE == "complementary") hueOffset *= 0.33
+    if (HUE_MODE == "triadic complementary") hueOffset *= 0.66
+    if (HUE_MODE == "tetradic complementary") hueOffset *= 0.75
+
+    if (HUE_MODE != "monochromatic") hueOffset += (Math.random() * 2 - 1) * 0.01
+
+    let chroma = chromaBase + linearIterator * chromaContrast
+    let lightness = lightnessBase + linearIterator * lightnessContrast
+
+    if (chromaConstant) chroma = chromaFixed
+    if (lightnessConstant) lightness = lightnessFixed
+
+    let lab = oklch_to_oklab(lightness, chroma, hueBase + hueOffset)
+    let rgb = oklab_to_linear_srgb(lab[0], lab[1], lab[2])
+
+    rgb[0] = Math.round(Math.max(0.0, Math.min(rgb[0], 1.0)) * 255)
+    rgb[1] = Math.round(Math.max(0.0, Math.min(rgb[1], 1.0)) * 255)
+    rgb[2] = Math.round(Math.max(0.0, Math.min(rgb[2], 1.0)) * 255)
+
+    oklchColors.push(rgb)
+  }
+
+  return oklchColors
+}
---
+++ sketch.js
@@ -0,0 +1,150 @@
+const State = {
+  div: 0,
+  palette: [],
+  lines: [],
+  shearX: 0
+}
+
+class Line {
+  constructor(color, stroke, y) {
+    this.color = color
+    this.stroke = stroke
+    this.y = y
+    this.ops = []
+    this.diverge_sign = sign(randomGaussian())
+    this.diverge_probability = 1.4
+    // shearX per line looks interesting
+    // shearY doesn't look good at all
+    // this.shearX = PI / (randomGaussian(1, 4) * 8)
+    // this.shearY = PI / (randomGaussian(1, 4) * 8)
+  }
+  toString () {
+    return `Line(${this.color}, ${this.y}, ${this.x1}, ${this.x2})`
+  }
+}
+
+function sign (value) {
+  const s = Math.sign(value)
+  if (s === 0) return 1
+  if (s === -0) return -1
+  return s
+}
+
+function absIntMod (value, mod) {
+  return Math.abs(parseInt(value)) % mod
+}
+
+function numericComp(a, b) { return a - b }
+
+function rr (max) {
+  return randomGaussian(max / State.div, 5)
+}
+
+function straightX (x, y, line, fix = null) {
+  let _x = fix == null ? x + Math.abs(rr(width)) : fix
+  _x = Math.min(_x, width)
+  line.ops.push(
+    [vertex, _x, y]
+  )
+  return [_x, y]
+}
+
+function divergeY (x, y, line) {
+  // using and inverting line.diverge_sign makes the lines return to their base
+  // instead of wandering off
+  let r = Math.abs(rr(height))
+  let x1 = x + r
+  let y1 = y + r * line.diverge_sign
+  let x2 = x1 + r
+  let y2 = y1 + r * line.diverge_sign
+  line.diverge_sign = -line.diverge_sign
+  line.ops.push(
+    [quadraticVertex, x1, y, x1, y1]
+  )
+  line.ops.push(
+    [quadraticVertex, x1, y2, x2, y2]
+  )
+  return [x2, y2]
+}
+
+function lineBezier (x, y, line) {
+  const r = randomGaussian()
+  if (r < -line.diverge_probability || r > line.diverge_probability) {
+    // increasing the probability makes the lines look less like a series of
+    // stairs
+    line.diverge_probability *= (line.diverge_probability * 0.95)
+    return divergeY(x, y, line)
+  }
+  return straightX(x, y, line)
+}
+
+function makeLineOps (line) {
+  let x = 0
+  let y = line.y
+  line.ops.push(
+    [vertex, x, y]
+  )
+  while (x < width) {
+    const [nx, ny] = lineBezier(x, y, line)
+    x = nx
+    y = ny
+  }
+  if (x < width) {
+    line.ops.push(
+      [vertex, width, y]
+    )
+  }
+  return line
+}
+
+function initState () {
+  State.div = parseInt(random(7, 21))
+  State.lines = new Array(parseInt(random(5, 13)))
+  State.shearX = PI / (randomGaussian(1, 4) * 8)
+  State.paletteSettings = PaletteSettings(State.lines.length + 1)
+  State.palette = generateOKLCH("triadic complementary", State.paletteSettings)
+
+  for (let i = 0, n = State.lines.length; i < n; i++) {
+    State.lines[i] = makeLineOps(
+      new Line(
+        i,
+        Math.abs(randomGaussian(n * n * 4) / Math.pow(i + 1, 2)),
+        absIntMod(randomGaussian(height / 2, 90), height)
+      )
+    )
+  }
+}
+
+function setup () {
+  createCanvas(1024, 768)
+  initState()
+}
+
+function draw () {
+  background(State.palette[0])
+  let j = 0;
+  for (let l of State.lines) {
+    push()
+    noFill()
+    strokeWeight(l.stroke + 4)
+    stroke(State.palette[0])
+    beginShape()
+    for (let [op, ...args] of l.ops) {
+      op(...args)
+    }
+    endShape()
+    strokeWeight(l.stroke)
+    stroke(State.palette[l.color + 1])
+    beginShape()
+    for (let [op, ...args] of l.ops) {
+      op(...args)
+    }
+    endShape()
+    pop()
+  }
+}
+
+window.addEventListener("click", (event) => {
+  initState()
+  //State.palette = generateOKLCH("triadic complementary", State.paletteSettings)
+})
